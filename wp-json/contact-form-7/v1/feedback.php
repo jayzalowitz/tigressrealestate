6ae1514089c71b648a31804f926ceb48a458d37e
Jay <jay@ramblist.com>
2022-01-24 04:15:27 -0500

    stuff

--- wp-json/contact-form-7/v1/feedback.php
+++ wp-json/contact-form-7/v1/feedback.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use SendGrid\Mail\Mail;
         $email;$comment;$captcha;$subject;
         if(isset($_POST['your-email'])){
