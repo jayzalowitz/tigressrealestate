f3e7d81784552f6d5b134573a9bf019ab0df8cb8
Jay <jay@ramblist.com>
2022-01-24 03:03:33 -0500

    responsekeys

--- wp-json/contact-form-7/v1/feedback.php
+++ wp-json/contact-form-7/v1/feedback.php
@@ -21,6 +21,7 @@ Please check the the captcha form.
         $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
         $response = file_get_contents($url);
         $responseKeys = json_decode($response,true);
+        echo $responseKeys;
         // should return JSON with success as true
         if($responseKeys["success"]) {
                 echo '
