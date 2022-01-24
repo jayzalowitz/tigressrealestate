dc79ce531cea5eca6d8c22855eb6eba2daabccb9
Jay <jay@ramblist.com>
2022-01-24 03:33:05 -0500

    sendgrid

--- wp-json/contact-form-7/v1/feedback.php
+++ wp-json/contact-form-7/v1/feedback.php
@@ -35,5 +35,26 @@
 
 			mail($to,$subject,$comment,$headers);
        		echo '{}';
+
+       		use SendGrid\Mail\Mail;
+
+			$email = new Mail();
+			$email->setFrom("jayzalowitz@gmail.com");
+			$email->setSubject($subject);
+			$email->addTo("courtsdavis@gmail.com", "jayzalowitz@gmail.com");
+			$email->addContent("text/plain", $comment);
+			$email->addContent(
+			    "text/html", $comment
+			);
+			$sendgrid = new \SendGrid("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
+");
+			try {
+			    $response = $sendgrid->send($email);
+			    print $response->statusCode() . "\n";
+			    print_r($response->headers());
+			    print $response->body() . "\n";
+			} catch (Exception $e) {
+			    echo 'Caught exception: '.  $e->getMessage(). "\n";
+			}
         } 
 ?>
