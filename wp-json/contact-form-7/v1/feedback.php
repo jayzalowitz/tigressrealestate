581be3a1176f07b80fb0f66753c00d54d20f0c7a
Jay <jay@ramblist.com>
2022-01-24 03:08:28 -0500

    more details

--- wp-json/contact-form-7/v1/feedback.php
+++ wp-json/contact-form-7/v1/feedback.php
@@ -20,7 +20,6 @@ Please check the the captcha form.
         $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
         $response = file_get_contents($url);
         $responseKeys = json_decode($response, false);
-        echo $response;
         // should return JSON with success as true
         if($responseKeys["success"]) {
                 echo '
